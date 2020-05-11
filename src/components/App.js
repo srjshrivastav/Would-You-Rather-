1a0202ddfe23a824f2c7b708774cd6b59c1f3b32
Suraj Shrivastava <srjshrivastav1@gmail.com>
2020-05-12 04:57:22 +0530

    Add a login Component

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,77 @@
 import React from "react";
 import logo from "../logo.svg";
 import "../App.css";
+import avatar from "../images/avatar.png";
+import { connect } from "react-redux";
+import { getInitialData } from "../utils/api";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img
-          src={logo}
-          className="App-logo"
-          alt="logo"
-          style={{ height: 100 }}
-        />
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    users: {},
+  };
+  componentDidMount() {
+    getInitialData().then(({ users, questions }) => {
+      this.setState(() => ({
+        users,
+      }));
+      console.log(users, questions);
+    });
+  }
+  render() {
+    const { users } = this.state;
+    return (
+      <div>
+        <nav className="navbar navbar-expand-lg bg-dark static-top ">
+          <a className="navbar-brand text-white" href="/">
+            WouldYouRather?
+          </a>
+        </nav>
+        <div className="container mt-5">
+          <div className="row row-content">
+            <div className="col-12 col-sm-5 ">
+              <div className="card text-center">
+                <div className="card-header bg-dark text-white">LOGIN</div>
+                <div className="card-body text-center">
+                  <div>
+                    <img src={avatar} alt="Avatar" className="avatar" />
+                  </div>
+                  <div className="btn-group dropright mt-3">
+                    <button type="button" className="btn btn-secondary">
+                      Select User
+                    </button>
+                    <button
+                      type="button"
+                      className="btn btn-secondary dropdown-toggle dropdown-toggle-split"
+                      data-toggle="dropdown"
+                      aria-haspopup="true"
+                      aria-expanded="false"
+                    >
+                      <span className="sr-only">Toggle Dropright</span>
+                    </button>
+                    <div className="dropdown-menu">
+                      {Object.keys(users).map((user) => (
+                        <a className="dropdown-item" key={user}>
+                          {user}
+                        </a>
+                      ))}
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="col-12 col-sm align-self-center order-sm-first">
+              <img
+                src={logo}
+                className="App-logo img-fluid"
+                alt="logo"
+                style={{ height: 100 }}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default App;
+export default connect()(App);
--- src/index.js
+++ src/index.js
@@ -1,11 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import reducer from "./reducers";
+import middleware from "./middlewares";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "jquery/dist/jquery.slim.min.js";
+import "popper.js/dist/popper.min.js";
+import "bootstrap/dist/js/bootstrap.min.js";
+import "./index.css";
 import App from "./components/App";
+import { BrowserRouter } from "react-router-dom";
 
+const store = createStore(reducer, middleware);
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
   document.getElementById("root")
 );
